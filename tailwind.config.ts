5feb14a193279ebbdb71c325723b296d95c6b9f0
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-10-06 14:35:57 +0100

    fix: styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,12 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './src/routes/**/*.{html,jsx, tsx}',
+    './src/components/**/*.{html,jsx, tsx}',
+  ],
   theme: {
     extend: {
       backgroundImage: {
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@
 import { defineConfig } from 'vite';
 import viteReact from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), viteReact()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
   build: {
     rollupOptions: {
       input: {
