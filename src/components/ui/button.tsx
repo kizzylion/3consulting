805b134735e9014560e8430b604bd80e80b69f91
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-10-06 23:21:34 +0100

    FEAT: ADDED BUTTON COMPONENT FOR EASY NAVIGATION

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,68 @@
+import { Link } from '@tanstack/react-router';
+import { cn } from '@utils/index';
+import React from 'react';
+
+type ButtonProps = {
+  children: React.ReactNode;
+  className?: string;
+  variant?: 'primary' | 'secondary' | 'outline';
+  onClick?: () => void;
+  activeOptions?: {
+    exact?: boolean;
+  };
+} & (
+  | { as: 'link'; to: string; className?: string }
+  | {
+      as?: 'button';
+      type?: 'button' | 'submit' | 'reset';
+    }
+);
+
+function Button({
+  children,
+  className,
+  variant = 'primary',
+  activeOptions,
+  onClick,
+  ...props
+}: ButtonProps) {
+  const baseStyles = 'font-semibold rounded-lg transition duration-300 ';
+  const variantStyles = {
+    primary: `${props.as === 'button' ? 'bg-accent-ui text-accent-dark-ui' : 'text-accent-ui'} hover:bg-yellow-600`,
+    secondary: 'bg-gray-200 text-gray-800 hover:bg-gray-300',
+    outline:
+      'bg-transparent border border-accent-ui text-accent-ui hover:bg-accent-ui hover:text-accent-dark-ui',
+  };
+
+  const combinedClassName = cn(baseStyles, variantStyles[variant], className);
+
+  if (props.as === 'link') {
+    return (
+      <Link
+        onClick={onClick}
+        activeOptions={activeOptions}
+        {...props}
+        className={combinedClassName}
+        activeProps={{
+          className: 'text-nav-active font-bold',
+        }}
+        preload='intent'
+      >
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <button
+      onClick={onClick}
+      className={cn('py-4 px-8 text-lg', combinedClassName, className)}
+      {...props}
+      type={props.type || 'button'}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
