b2e79ebce12bab68988c4f6f2d0659f6b6000a0c
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-10-06 18:16:30 +0100

    fix: fix path config

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import viteReact from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), viteReact()],
+  plugins: [TanStackRouterVite(), viteReact(), tsconfigPaths()],
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
