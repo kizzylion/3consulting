41151461bd8508bb64b1ceefcaf17c30aa11dd42
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-10-06 18:19:35 +0100

    feat: added style branding and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,15 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        'accent-ui': '#FDB022',
+        'text-ui': '#4E1D09',
+        'text-main': '#344054',
+        'text-hero': 'linear-gradient(to right, #DC6803, #FDB022)',
+        'nav-idle': '#EC4A0A',
+        'nav-active': '#FDB022',
+        'text-heading': '#101828',
+      },
       backgroundImage: {
         'grid-pattern':
           'linear-gradient(to right, #8080800a 1px, transparent 1px), linear-gradient(to bottom, #8080800a 1px, transparent 1px)',
