44778d7f71c273311497071f3d19fd2730554e25
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-10-19 03:58:52 +0100

    feat: scroll animation and microinteraction

--- src/layout/view.tsx
+++ src/layout/view.tsx
@@ -1,7 +1,24 @@
+"use client";
+import { useEffect } from "react";
 import { Outlet } from '@tanstack/react-router';
 import NavBar from '@components/ui/nav-bar';
+import Lenis from "lenis"
 
 function View() {
+  // we need this for smooth animation on scroll
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: any) {
+      lenis.raf(time);
+
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
+
   return (
     <div className='h-full mx-auto'>
       <div className='z-20 fixed w-full bg-white h-fit border-b border-gray-300'>
