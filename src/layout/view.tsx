64062ef7fc8f07dc603b975d3b1fa6fc9a35b79b
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-10-23 17:28:19 +0100

    FEAT: CHANGED SCROLL BEHAVIOUR

--- src/layout/view.tsx
+++ src/layout/view.tsx
@@ -1,45 +1,11 @@
 "use client";
-import { useEffect, useRef, createContext } from "react";
-import { Outlet, useLocation } from '@tanstack/react-router';
-import NavBar from '@components/ui/nav-bar';
-import Lenis from "lenis"
 
-export const LenisContext = createContext<Lenis | null>(null);
+import { Outlet } from '@tanstack/react-router';
+import NavBar from '@components/ui/nav-bar';
 
 function View() {
-  const lenisRef = useRef<Lenis | null>(null);
-  const location = useLocation();
-
-  useEffect(() => {
-    lenisRef.current = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-
-
-      touchMultiplier: 2,
-    });
-
-    function raf(time: number) {
-      lenisRef.current?.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-
-    // Clean up function
-    return () => {
-      lenisRef.current?.destroy();
-    };
-  }, []);
-
-  useEffect(() => {
-    // Reset Lenis scroll position and recalculate on route change
-    lenisRef.current?.scrollTo(0, { immediate: true });
-    lenisRef.current?.resize();
-  }, [location.pathname]);
-
   return (
-    <LenisContext.Provider value={lenisRef.current}>
+    <>
       <div className='min-h-screen flex flex-col'>
         <div className='z-20 sticky top-0 bg-white border-b border-gray-300'>
           <NavBar />
@@ -48,7 +14,7 @@ function View() {
           <Outlet />
         </main>
       </div>
-    </LenisContext.Provider>
+    </>
   );
 }
 
