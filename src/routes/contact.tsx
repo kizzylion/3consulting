2afc70a057e9c48ecf54fb8bc679a1d850a2bd98
kizzylion <kztchm@gmail.com>
2024-10-23 11:48:01 -0400

    FEAT: ADD CONTACT US FORM AND FOOTER  TO THE CONTACT US PAGE

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -1,9 +1,16 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute } from '@tanstack/react-router'
+import ContactUs from '@components/pages/contactus/ContactUs'
+import Footer from '@components/ui/Footer'
 
 export const Route = createFileRoute('/contact')({
   component: Contact,
-});
+})
 
 function Contact() {
-  return <div className='p-4'>Contact us here.</div>;
+  return (
+    <>
+      <ContactUs />
+      <Footer />
+    </>
+  )
 }
